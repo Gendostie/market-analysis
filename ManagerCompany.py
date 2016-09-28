72df82a2341c9b16709490823fb097856523dda1
GenevieveDostie <gendostie@hotmail.com>
2016-09-28 17:03:51 -0400

    - Create function to insert all s&p500 in table company - ManagerCompany: all function can take a connexion already create

--- ManagerCompany.py
+++ ManagerCompany.py
@@ -9,8 +9,15 @@ PASSWORD = 'root'
 DATABASE = 'market_analysis'
 
 
-def get_snp500():
-    db = DBConnection(HOST, USER, PASSWORD, DATABASE)
+def get_snp500(db=None):
+    """
+    Get company with flag is_in_snp500 in db
+    :param db: if we have already connexion in other function who cal this function
+    :type db: DBConnection
+    :return:
+    """
+    if not db:
+        db = DBConnection(HOST, USER, PASSWORD, DATABASE)
     query = """SELECT name, symbol, last_update_historic
                 FROM company WHERE is_in_snp500 = 1"""
     res = db.select_in_db(query)
@@ -18,38 +25,44 @@ def get_snp500():
     return res
 
 
-def add_company_to_db(symbol, name):
+def add_company_to_db(symbol, name, db=None):
     """
     add a company to table company
     :param symbol: symbol of company in stock market
     :type symbol: string
     :param name: name of company in stock market
     :type name: string
+    :param db: if we have already connexion in other function who cal this function
+    :type db: DBConnection
     :return: None
     """
     if symbol is None or name is None:
         raise ValueError('Name or symbol is None. name: %(name)s ; symbol: %(symbol)s ' % locals())
 
-    db = DBConnection(HOST, USER, PASSWORD, DATABASE)
+    if not db:
+        db = DBConnection(HOST, USER, PASSWORD, DATABASE)
     # Add new company not in table and update name and flag snp500 if already in table
-    query = """INSERT INTO company (name, symbol, is_in_snp_500) VALUES (%(name)s, %(symbol)s, 1)
-                ON DUPLICATE KEY UPDATE name = name AND is_in_snp500 = 1"""
-
+    query = """INSERT INTO company (name, symbol, is_in_snp500) VALUES (UPPER(%(name)s), %(symbol)s, 1)
+                ON DUPLICATE KEY UPDATE name = %(name)s, is_in_snp500 = 1"""
     db.modified_db(query, {'name': name, 'symbol': symbol})
     db.close_connection()
 
 
-def get_company_by_symbol(symbol):
+def get_company_by_symbol(symbol, db=None):
     """
 
     :param symbol: symbol of a company we search
     :type symbol: string
+    :param db: if we have already connexion in other function who cal this function
+    :type db: DBConnection
     :return: return tuple of company who correspond to symbol
     :rtype: tuple
     """
     if symbol is None:
         raise ValueError('Symbol is None.')
-    db = DBConnection(HOST, USER, PASSWORD, DATABASE)
+
+    if not db:
+        db = DBConnection(HOST, USER, PASSWORD, DATABASE)
     query = """SELECT name, symbol, is_in_snp500, last_update_historic
                 FROM company WHERE symbol = %(symbol)s"""
     res = db.select_in_db(query, {'symbol': symbol})
@@ -57,17 +70,21 @@ def get_company_by_symbol(symbol):
     return res
 
 
-def get_company_by_name(name):
+def get_company_by_name(name, db=None):
     """
 
     :param name: name of a company we search
     :type name: string
+    :param db: if we have already connexion in other function who cal this function
+    :type db: DBConnection
     :return: return tuple of company who correspond to name
     :rtype: tuple
     """
     if name is None:
         raise ValueError('Name is None.')
-    db = DBConnection(HOST, USER, PASSWORD, DATABASE)
+
+    if not db:
+        db = DBConnection(HOST, USER, PASSWORD, DATABASE)
     query = """SELECT name, symbol, is_in_snp500, last_update_historic
                 FROM company WHERE name = %(name)s"""
     res = db.select_in_db(query, {'name': name})
@@ -75,16 +92,22 @@ def get_company_by_name(name):
     return res
 
 
-def update_snp550_to_db():
+def update_snp550_to_db(db=None):
     """
     Update data of table company to check if we have new company and remove company not in new list s&p500
-    :return:
+    :param db: if we have already connexion in other function who cal this function
+    :type db: DBConnection
+    :return: None
     """
-    db = DBConnection(HOST, USER, PASSWORD, DATABASE)
+    if not db:
+        db = DBConnection(HOST, USER, PASSWORD, DATABASE)
     # Set all company in table for to re-init to 0 flag is_in_snp500
     query = """UPDATE company SET is_in_snp500 = 0"""
+    db.modified_db(query)
 
-    snp500 = finsymbols.get_sp500_symbols()
+    snp500 = finsymbols.get_sp500_symbols(db)
     # update and add_company of s500
     for company in snp500:
         add_company_to_db(company.get('symbol'), company.get('company'))
+
+    # TODO: call fct to historic data in csv [Gen]
--- initialize_db.py
+++ initialize_db.py
@@ -5,4 +5,11 @@ import finsymbols
 
 
 def insert_company_snp500():
+    """
+    Insert company s&p500 in table company
+    :return: None
+    """
     snp500 = finsymbols.get_sp500_symbols()
+
+    for company in snp500:
+        ManagerCompany.add_company_to_db(company.get('symbol'), company.get('company'))
