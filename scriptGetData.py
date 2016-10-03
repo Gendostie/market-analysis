e06e4df46b974371eca3706fe9ff168a305fe480
gdostie <gendostie@hotmail.com>
2016-10-02 21:19:38 -0400

    Correct format of scripts to respect PEP8

--- scriptGetData.py
+++ scriptGetData.py
@@ -6,7 +6,7 @@ PATH = os.getcwd() + DIR_NAME
 DEBUG = False
 
 if DEBUG:
-	os.rename(PATH + 'log.txt', PATH + 'copylog.txt')
+    os.rename(PATH + 'log.txt', PATH + 'copylog.txt')
 
 # Get all symbols of the S&P500
 sp500 = finsymbols.get_sp500_symbols()
@@ -17,19 +17,18 @@ suffix = '&region=USA&culture=en-CA&cur=&order=asc'
 
 # Log when we receive nothing for a company
 with open(PATH + 'log.txt', 'a') as logFile:
-	for i in range(len(sp500)):
-		symbol = sp500[i]['symbol']
-		company = sp500[i]['company']
+    for i in range(len(sp500)):
+        symbol = sp500[i]['symbol']
+        company = sp500[i]['company']
 
-		# Replace the '-' by a dot for the URL request
-		symbolURL = symbol.replace('-', '.')
+        # Replace the '-' by a dot for the URL request
+        symbolURL = symbol.replace('-', '.')
 
-		# Try one time to get the CSV. Write in the file if we receive something.
-		with open(PATH + symbol + '.csv', 'w') as csvFile:
-			csvData = requests.get(prefix + symbolURL + suffix)
-			if csvData.text:
-				csvFile.write(csvData.text) 
-			else:
-				logFile.write("{} = An empty string was received for {} ({}) \n" \
-					.format(strftime("%d %b %Y %H:%M:%S", localtime()), symbolURL, company))
-
+        # Try one time to get the CSV. Write in the file if we receive something.
+        with open(PATH + symbol + '.csv', 'w') as csvFile:
+            csvData = requests.get(prefix + symbolURL + suffix)
+            if csvData.text:
+                csvFile.write(csvData.text)
+            else:
+                logFile.write("{} = An empty string was received for {} ({}) \n" \
+                              .format(strftime("%d %b %Y %H:%M:%S", localtime()), symbolURL, company))
