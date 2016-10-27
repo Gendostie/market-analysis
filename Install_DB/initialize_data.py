51779d84287b85cabe4e8f58964d939ad6ab1209
j-philippe-rochette <jp.roch9@gmail.com>
2016-10-27 14:34:00 -0400

    Fetching is cleaned up. Still working on the update section.

--- Install_DB/initialize_data.py
+++ Install_DB/initialize_data.py
@@ -10,6 +10,7 @@ from datetime import datetime
 from time import localtime, strftime
 from Manager_DB.DbConnection import DBConnection
 from Manager_DB.ManagerCompany import insert_historic_value_to_db, insert_daily_value_to_db, insert_dividend_to_db
+import sys
 
 
 ################################################################################################
@@ -20,62 +21,91 @@ from Manager_DB.ManagerCompany import insert_historic_value_to_db, insert_daily_
 ################################################################################################
 
 
-# TODO : Add a comment
-def get_all_csv(get_histo=True, get_daily=True, get_div=True):
-    # Get all symbols of the S&P500
+def get_csv(is_fetching_histo=True, is_fetching_daily=True, is_fetching_div=True):
+    """Intermediary function in charge of centralizing the fetching of the selected CSVs.
+
+    :param is_fetching_histo: If True, the historical information will be downloaded from MorningStar. Default is True.
+    :param is_fetching_daily: If True, the daily information will be downloaded from Yahoo Finance. Default is True.
+    :param is_fetching_div: If True, the dividend information will be downloaded from Yahoo Finance. Default is True.
+    :return: Nothing. All CSVs downloaded will be found in the directory SNP500 located in the project's directory.
+    """
+    # Get all symbols of the S&P500.
+    # TODO: Get all the companies that were in the S&P500 between two dates. Survivor bias.
     sp500 = finsymbols.get_sp500_symbols()
 
-    # Get the CSVs from MorningStar for the historical information.
-    if get_histo:
-        get_all_historical(sp500)
-#    else:
-#        print("MorningStar's CSV were not queried.")
+    # Get the configuration file
+    config = configparser.ConfigParser()
+    config.read('../config.ini')
 
-    # Get the CSVs from Yahoo Finance for the daily information.
-    if get_daily:
-        get_all_daily(sp500)
-#    else:
-#        print("Yahoo Finance's CSV were not queried.")
-    if get_div:
-        get_all_dividend(sp500)
+    # Open the log file for logging the errors that might encounter along the way.
+    logfile = open(config.get('path', 'PATH_LOG'), 'a')
+
+    if is_fetching_histo:
+        get_all_historical(sp500, config, logfile)
+
+    if is_fetching_daily:
+        get_all_daily(sp500, config, logfile)
+
+    if is_fetching_div:
+        get_all_dividend(sp500, config, logfile)
+
+    logfile.close()
+    # TODO : Any update to do to config?
 
 
-# TODO : Add a verification to download only what is needed.
-def get_all_historical(sp500):
-    # Fetch the CSVs from MorningStar and save the content in a CSV file
+def get_all_historical(sp500, config, logfile):
+    """Fetch the CSVs from MorningStar and save the content in a CSV file.
+
+    All CSVs are named following the format: "histo_" + the company's symbol + ".csv"
+    ex: For Google, the CSV's name is "histo_GOOGL.csv".
+
+    Log any exceptions raised while querying MorningStar.
+
+    :param sp500: List of unique S&P500's companies' symbols.
+    :param config: An open configparser
+    :param logfile: An open text file used as a log.
+    :return: Nothing. All CSVs downloaded will be found in the directory SNP500 located in the project's directory.
+    """
     prefix = 'http://financials.morningstar.com/ajax/exportKR2CSV.html?&callback=?&t='
     suffix = '&region=USA&culture=en-CA&cur=&order=asc'
 
-    config = configparser.ConfigParser()
-    config.read('../config.ini')
-
-    log_path = config.get('path', 'PATH_LOG')
     dir_path = config.get('path', 'PATH_SNP500')
 
-    # Log when we receive nothing for a company
-    with open(log_path, 'a') as logFile:
-        for i in range(len(sp500)):
-            symbol = sp500[i]['symbol']
-            company = sp500[i]['company']
+    for i in range(len(sp500)):
+        symbol = sp500[i]['symbol']
+        company = sp500[i]['company']
 
-            # Replace the '-' by a dot for the URL request
-            symbol_url = symbol.replace('-', '.')
+        # Replace the '-' by a dot for the URL request
+        symbol_url = symbol.replace('-', '.')
 
-            # Try one time to get the CSV. Write in the file if we receive something.
-            with open(dir_path + "histo_" + symbol + '.csv', 'w') as csvFile:
+        with open(dir_path + "histo_" + symbol + '.csv', 'w') as csvFile:
+            try:
                 r = requests.get(prefix + symbol_url + suffix)
-                if r.text:
-                    csvFile.write(r.text)
-                else:
-                    logFile.write("{} = An empty string was received for {} ({}) \n"
-                                  .format(strftime("%d %b %Y %H:%M:%S", localtime()), symbol_url, company))
+            except:
+                logfile.write("{} [FETCH][HISTO] Unexpected error for {} ({}): {} {}\n"
+                              .format(strftime("%d %b %Y %H:%M:%S", localtime()), symbol, company,
+                                      sys.exc_info()[0], sys.exc_info()[1]))
+
+            # In the case that we receive an empty file (quite possible actually), we don't want to overwrite an
+            #     existing file containing information.
+            if r.text:
+                csvFile.write(r.text)
 
 
-def get_all_daily(sp500):
-    # Get all the configuration from the config.ini file
-    config = configparser.ConfigParser()
-    config.read('../config.ini')
+def get_all_daily(sp500, config, logfile):
+    # TODO: Data max and min
+    """Fetch the CSVs from Yahoo Finance and save the content in a CSV file.
 
+    All CSVs are named following the format: "daily_" + the company's symbol + ".csv"
+    ex: For Google, the CSV's name is "daily_GOOGL.csv".
+
+    Log any exceptions raised while querying Yahoo Finance.
+
+    :param sp500: List of unique S&P500's companies' symbols.
+    :param config: An open configparser
+    :param logfile: An open text file used as a log.
+    :return: Nothing. All CSVs downloaded will be found in the directory SNP500 located in the project's directory.
+    """
     day_max = config.get('daily', 'DAY_MAX')
     month_max = config.get('daily', 'MONTH_MAX')
     year_max = config.get('daily', 'YEAR_MAX')
@@ -84,36 +114,44 @@ def get_all_daily(sp500):
     month_min = config.get('daily', 'MONTH_MIN')
     year_min = config.get('daily', 'YEAR_MIN')
 
-    log_path = config.get('path', 'PATH_LOG')
     dir_path = config.get('path', 'PATH_SNP500')
 
-    # Fetch the CSVs from MorningStar and save the content in a CSV file
     prefix = "http://real-chart.finance.yahoo.com/table.csv?s="
     suffix = "&d={}&e={}&f={}&g=d&a={}&b={}&c={}&ignore=.csv".format(day_max, month_max, year_max,
                                                                      day_min, month_min, year_min)
 
-    # Log when we receive nothing for a company
-    # TODO : Add verification that the file is a CSV.
-    with open(log_path, 'a') as logFile:
-        for i in range(len(sp500)):
-            symbol = sp500[i]['symbol']
-            company = sp500[i]['company']
+    for i in range(len(sp500)):
+        symbol = sp500[i]['symbol']
+        company = sp500[i]['company']
 
-            # Try one time to get the CSV. Write in the file if we receive something.
-            with open(dir_path + "daily_" + symbol + '.csv', 'w') as csvFile:
+        with open(dir_path + "daily_" + symbol + '.csv', 'w') as csvFile:
+            try:
                 r = requests.get(prefix + symbol + suffix)
-                if r.text:
-                    csvFile.write(r.text)
-                else:
-                    logFile.write("{} = An empty string was received for {} ({}) \n"
-                                  .format(strftime("%d %b %Y %H:%M:%S", localtime()), symbol, company))
+            except:
+                logfile.write("{} [FETCH][DAILY] Unexpected error for {} ({}): {} {}\n"
+                              .format(strftime("%d %b %Y %H:%M:%S", localtime()), symbol, company,
+                                      sys.exc_info()[0], sys.exc_info()[1]))
+
+            # In the case that we receive an empty file, we don't want to overwrite an existing file
+            #      containing information.
+            if r.text:
+                csvFile.write(r.text)
 
 
-def get_all_dividend(sp500):
-    # Get all the configuration from the config.ini file
-    config = configparser.ConfigParser()
-    config.read('../config.ini')
+def get_all_dividend(sp500, config, logfile):
+    # TODO: Data max and min
+    """Fetch the CSVs from Yahoo Finance and save the content in a CSV file.
 
+    All CSVs are named following the format: "div_" + the company's symbol + ".csv"
+    ex: For Google, the CSV's name is "daily_GOOGL.csv".
+
+    Log any exceptions raised while querying Yahoo Finance.
+
+    :param sp500: List of unique S&P500's companies' symbols.
+    :param config: An open configparser
+    :param logfile: An open text file used as a log.
+    :return: Nothing. All CSVs downloaded will be found in the directory SNP500 located in the project's directory.
+    """
     day_max = config.get('daily', 'DAY_MAX')
     month_max = config.get('daily', 'MONTH_MAX')
     year_max = config.get('daily', 'YEAR_MAX')
@@ -122,41 +160,39 @@ def get_all_dividend(sp500):
     month_min = config.get('daily', 'MONTH_MIN')
     year_min = config.get('daily', 'YEAR_MIN')
 
-    log_path = config.get('path', 'PATH_LOG')
     dir_path = config.get('path', 'PATH_SNP500')
 
-    # Fetch the CSVs from MorningStar and save the content in a CSV file
     prefix = "http://real-chart.finance.yahoo.com/table.csv?s="
     suffix = "&a={}&b={}&c={}&d={}&e={}&f={}&g=v&ignore=.csv".format(month_min, day_min, year_min,
                                                                      month_max, day_max, year_max)
 
-    # Log when we receive nothing for a company
-    # TODO : Add verification that the file is a CSV.
-    with open(log_path, 'a') as logFile:
-        for i in range(len(sp500)):
-            symbol = sp500[i]['symbol']
-            company = sp500[i]['company']
+    for i in range(len(sp500)):
+        symbol = sp500[i]['symbol']
+        company = sp500[i]['company']
 
-            # Try one time to get the CSV. Write in the file if we receive something.
-            with open(dir_path + "div_" + symbol + '.csv', 'w') as csvFile:
+        with open(dir_path + "div_" + symbol + '.csv', 'w') as csvFile:
+            try:
                 r = requests.get(prefix + symbol + suffix)
-                if r.text:
-                    csvFile.write(r.text)
-                else:
-                    logFile.write("{} = An empty string was received for {} ({}) \n"
-                                  .format(strftime("%d %b %Y %H:%M:%S", localtime()), symbol, company))
+            except:
+                logfile.write("{} [FETCH][DIVID] Unexpected error for {} ({}): {} {}\n"
+                              .format(strftime("%d %b %Y %H:%M:%S", localtime()), symbol, company,
+                                      sys.exc_info()[0], sys.exc_info()[1]))
+
+            # In the case that we receive an empty file, we don't want to overwrite an existing file
+            #      containing information.
+            if r.text:
+                csvFile.write(r.text)
 
 
 ################################################################################################
 #
-#                                   SET UP DATABASE FROM SCRATCH
+#                                   UPDATE DATABASE WITH THE CSVs
 #
 #
 ################################################################################################
 
 
-# TODO : Add a verification to update only what is needed
-def set_up_all_with_csv(set_histo=True, set_daily=True, set_div=True):
+def update_with_csv(is_updating_histo=True, is_updating_daily=True, is_updating_div=True):
     config = configparser.ConfigParser()
     config.read('../config.ini')
 
@@ -168,44 +204,37 @@ def set_up_all_with_csv(set_histo=True, set_daily=True, set_div=True):
                       config.get('database', 'PASSWORD'),
                       config.get('database', 'DATABASE'))
 
-    with open(log_path, 'a') as logFile:
-        # Updating with historical data
-        if set_histo:
-            for filename in glob.glob(dir_path + 'histo_*.csv'):
-                # If the file is not empty, call the function in charge of the update.
-                if os.stat(filename).st_size != 0:
-                    set_up_historical(filename, db)
-                else:
-                    logFile.write("{} = The file \"{}\" was empty.\n" \
-                                  .format(strftime("%d %b %Y %H:%M:%S", localtime()), filename))
+    logfile = open(log_path, 'a')
 
-        # Updating with daily data
-        if set_daily:
-            for filename in glob.glob(dir_path + 'daily_*.csv'):
-                # If the file is not empty, call the function in charge of the update.
-                if os.stat(filename).st_size != 0:
-                    set_up_daily(filename, db)
-                else:
-                    logFile.write("{} = The file \"{}\" was empty.\n" \
-                                  .format(strftime("%d %b %Y %H:%M:%S", localtime()), filename))
+    # with open(log_path, 'a') as logfile:
+    if is_updating_histo:
+        for filename in glob.glob(dir_path + 'histo_*.csv'):
+            if os.stat(filename).st_size != 0:
+                update_historical(filename, config, db)
+            else:
+                logfile.write("{} [UPDATE][HISTO] Skipped \"{}\" because the file was empty.\n"
+                              .format(strftime("%d %b %Y %H:%M:%S", localtime()), filename))
 
-        # Updating with dividend data
-        if set_div:
-            for filename in glob.glob(dir_path + 'div_*.csv'):
-                # If the file is not empty, call the function in charge of the update.
-                if os.stat(filename).st_size != 0:
-                    set_up_dividend(filename, db)
-                else:
-                    logFile.write("{} = The file \"{}\" was empty.\n" \
-                                  .format(strftime("%d %b %Y %H:%M:%S", localtime()), filename))
+    if is_updating_daily:
+        for filename in glob.glob(dir_path + 'daily_*.csv'):
+            if os.stat(filename).st_size != 0:
+                update_daily(filename, db)
+            else:
+                logfile.write("{} [UPDATE][DAILY] Skipped \"{}\" because the file was empty.\n"
+                              .format(strftime("%d %b %Y %H:%M:%S", localtime()), filename))
+
+    if is_updating_div:
+        for filename in glob.glob(dir_path + 'div_*.csv'):
+            if os.stat(filename).st_size != 0:
+                update_dividend(filename, db)
+            else:
+                logfile.write("{} [UPDATE][DIVID] Skipped \"{}\" because the file was empty.\n"
+                              .format(strftime("%d %b %Y %H:%M:%S", localtime()), filename))
+
+    logfile.close()
 
 
-def set_up_historical(filename, db):
-    """
-
-    :param filename:
-    :return:
-    """
+def update_historical(filename, config, db):
     histo_funct = {'Revenue USD Mil': lambda x: None if pd.isnull(x) else int(x.replace(",", "")),
                    'Gross Margin %': float,
                    'Net Income USD Mil': lambda x: None if pd.isnull(x) else int(x.replace(",", "")),
@@ -214,15 +243,14 @@ def set_up_historical(filename, db):
                    'Book Value Per Share * USD': float,
                    'Free Cash Flow Per Share * USD': float}
 
-    config = configparser.ConfigParser()
-    config.read('../config.ini')
-
     log_path = config.get('path', 'PATH_LOG')
 
     # The dates have the format YYYY-MM in the CVSs. We want to remove the TTM and errors if there are any.
     reg_expr = re.compile('.{4}-.{2}')
 
     df = pd.read_csv(filename, header=2, index_col=0)
+
+    # Take only the rows that we need. The list containing the names of those rows is in the configuration file.
     df = df.loc[json.loads(config.get('list', 'HISTO_ROWS'))]
 
     # Cut the filename to get the symbol of the company; which is always the name of the CSV file.
@@ -257,7 +285,7 @@ def set_up_historical(filename, db):
                 insert_historic_value_to_db(str(symbol), query_params, db)
 
 
-def set_up_daily(filename, db):
+def update_daily(filename, db):
     # TODO : Not using the log file
     config = configparser.ConfigParser()
     config.read('../config.ini')
@@ -285,7 +313,7 @@ def set_up_daily(filename, db):
             insert_daily_value_to_db(str(symbol), query_params, db)
 
 
-def set_up_dividend(filename, db):
+def update_dividend(filename, db):
     # TODO : Not using the log file
     config = configparser.ConfigParser()
     config.read('../config.ini')
