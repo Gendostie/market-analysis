3e1dfb6346fe54837773d9843ac2b089f9d908b9
gdostie <gendostie@hotmail.com>
2016-11-14 13:49:55 -0500

    test for create pop-up for a type simulator selected and get value params

---
+++ QT/ManagerTest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from PyQt4 import QtCore, QtGui
+
+from QT.test1 import Ui_Dialog
+
+class ManagerTest(Ui_Dialog):
+
+
+    def setup_manager(self):
+        self.buttonBox.accepted.connect(self.get_value)
+
+    def get_value(self):
+        return 'test'
